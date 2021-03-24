3185b5a10eaadd01943d8c0d13b356ed29b1d919
sivakumarprsmy <sivakumarprsmy@gmail.com>
2021-03-24 16:23:40 -0400

    Removing the redundant, dead code.

--- src/com/ds/sorting/BubbleSort.java
+++ src/com/ds/sorting/BubbleSort.java
@@ -28,9 +28,6 @@ public class BubbleSort {
     }
 
     public static void swap(int[] array, int i, int j){
-        if(i == j){
-            return;
-        }
 
         int temp = array[i];
         array[i] = array[j];
